678868a6e02e02269403934f36cde27713dd57f2
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-08-28 12:11:33 +0200

    Skip mobile tests for now

--- test.js
+++ test.js
@@ -5,13 +5,13 @@ const isDarwin = Bare.platform === 'darwin'
 const isLinux = Bare.platform === 'linux'
 const isWindows = Bare.platform === 'win32'
 
-test('ios', { skip: !isDarwin }, async (t) => {
+test.skip('ios', { skip: !isDarwin }, async (t) => {
   await run(require.resolve('./test/fixtures/basic/index.js'), {
     platform: 'ios'
   })
 })
 
-test('android', { skip: !isDarwin && !isLinux }, async (t) => {
+test.skip('android', { skip: !isDarwin && !isLinux }, async (t) => {
   await run(require.resolve('./test/fixtures/basic/index.js'), {
     platform: 'android'
   })
